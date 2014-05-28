c7887276420597d12c01a4ae53193f30a8007f4e
MiniCoft <MiniCoft@gmail.com>
2014-05-28 22:27:26 +0800

    Console Udp Socket

---
+++ CSocket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net.Sockets;
+using System.Net;
+
+using System.Threading;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Target IP(Ex:127.0.0.1):");
+            string ip = Console.ReadLine();
+
+            Console.WriteLine("Your/Target Port(Ex:8888):");
+            int port = Convert.ToInt32(Console.ReadLine());
+
+            cSocket s = new cSocket(ip, port);
+            s.Start();
+
+        }
+        
+    }
+}
---
+++ CSocket/cSocket.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net.Sockets;
+using System.Net;
+
+using System.Threading;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CSocket
+{
+    
+    class cSocket
+    {
+        int mRecvBuffer = 20;
+        string ip;
+        int port;
+        public cSocket(string _ip, int _port)
+        {
+            ip = _ip;
+            port = _port;
+        }
+        public void Start()
+        {
+            Thread thread = new Thread(new ThreadStart(SocketListener));
+            thread.Start();
+            while (true)
+            {
+                string s = Console.ReadLine();
+                if (s == "exit")
+                    break;
+                if (s.Length > mRecvBuffer)
+                {
+                    Console.WriteLine("This Sting is Over " + mRecvBuffer);
+                    continue;
+                }
+                Send(s);
+            }
+            thread.Abort();
+        }
+        void SocketListener()
+        {
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, port);
+            Socket mSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            mSocket.Bind(endpoint);
+
+            EndPoint Remote = (EndPoint)(endpoint);
+            while (true)
+            {
+                int recv;
+                byte[] bytes = new byte[mRecvBuffer];
+                recv = mSocket.ReceiveFrom(bytes, ref Remote);
+
+                BinaryFormatter bf = new BinaryFormatter();
+                MemoryStream stream = new MemoryStream(bytes);
+                string s = System.Text.Encoding.Default.GetString(bytes);
+
+                Console.WriteLine("Recv:" + s);
+
+            }
+
+        }
+
+
+        public void Send(string msg)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            byte[] bytes = System.Text.Encoding.Default.GetBytes(msg);
+            s.SendTo(bytes, bytes.Length, SocketFlags.None, endPoint);
+        }
+    }
+}
